c9965c9800b39c1435ed3939a115fc442600539e
Song Lee <songlish233@gmail.com>
2024-03-31 10:42:10 +0900

    infinite corn farming

--- Farm Sim Proto1/Assets/Scripts/PlotManager.cs
+++ Farm Sim Proto1/Assets/Scripts/PlotManager.cs
@@ -5,7 +5,28 @@ using UnityEngine;
 public class PlotManager : MonoBehaviour
 {
     private bool isPlanted;
-    [SerializeField] private GameObject plant;
+
+    [SerializeField] private Sprite[] plantSprites;
+    [SerializeField] private SpriteRenderer plant;
+
+    private int plantStage;
+    private float timeBtwStages = 0.5f;
+    private float timer;
+
+    private void Update()
+    {
+        if (isPlanted)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer < 0 && plantStage < plantSprites.Length - 1)
+            {
+                plantStage++;
+                timer = timeBtwStages;
+                UpdatePlant();
+            }
+        }
+    }
 
     private void OnMouseDown()
     {
@@ -22,13 +43,20 @@ public class PlotManager : MonoBehaviour
     {
         Debug.Log("Harvested!");
         isPlanted = false;
-        plant.SetActive(false);
+        plant.gameObject.SetActive(false);
     }
 
     private void Plant()
     {
         Debug.Log("Planted!");
         isPlanted = true;
-        plant.SetActive(true);
+        plantStage = 0;
+        UpdatePlant();
+        plant.gameObject.SetActive(true);
+    }
+
+    private void UpdatePlant()
+    {
+        plant.sprite = plantSprites[plantStage];
     }
 }
